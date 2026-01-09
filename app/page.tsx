6615163bb85bcf2f0dba30039977eb9a9de02b44
fabrisdev <ezepuntocom@hotmail.com>
2026-01-09 20:50:29 -0300

    Added menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,42 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [menu, setMenu] = useState({
+    visible: false,
+    x: 0,
+    y: 0,
+  });
+
+  function handleContextMenu(event: React.MouseEvent) {
+    event.preventDefault();
+    setMenu({
+      visible: true,
+      x: event.clientX,
+      y: event.clientY,
+    });
+  }
+
+  function handleCloseContextMenu() {
+    setMenu({
+      ...menu,
+      visible: false,
+    });
+  }
+
   return (
-    <main className="bg-[url(/backgrounds/blobs-d.svg)] h-svh bg-cover bg-center"></main>
+    <main
+      className="bg-[url(/backgrounds/blobs-d.svg)] h-svh bg-cover bg-center"
+      onContextMenu={handleContextMenu}
+      onClick={handleCloseContextMenu}
+      onKeyUp={() => {}}
+    >
+      {menu.visible && (
+        <div className="fixed" style={{ left: menu.x, top: menu.y }}>
+          <p>Menu</p>
+        </div>
+      )}
+    </main>
   );
 }
