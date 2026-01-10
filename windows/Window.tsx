33a44116b854ef1413a58cdd80498ad200a07ea8
fabrisdev <ezepuntocom@hotmail.com>
2026-01-10 01:26:41 -0300

    Fix wrong property names

--- windows/Window.tsx
+++ windows/Window.tsx
@@ -9,13 +9,14 @@ export function Window({
   children,
   className,
   position,
+  id,
 }: PropsWithChildren<Props>) {
   return (
     <div
       className={`fixed bg-[#36363a] p-1.5 rounded-2xl border border-[#424247] text-[15px] ${className}`}
       style={{
-        x: position.x,
-        y: position.y,
+        left: position.x,
+        top: position.y,
       }}
     >
       {children}
