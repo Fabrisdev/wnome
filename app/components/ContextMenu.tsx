9c3bcac311efe8ac782a0d97dedb9eb28fee867b
fabrisdev <ezepuntocom@hotmail.com>
2026-01-09 22:26:38 -0300

    Workaround while i work on proper settings menu

--- app/components/ContextMenu.tsx
+++ app/components/ContextMenu.tsx
@@ -1,3 +1,5 @@
+import { useBackground } from "@/hooks/useBackground";
+import type { Background } from "@/stores/background";
 import { Button } from "./Button";
 
 type Props = {
@@ -7,6 +9,7 @@ type Props = {
 };
 
 export function ContextMenu({ x, y, visible }: Props) {
+  const background = useBackground();
   if (!visible) return null;
 
   return (
@@ -14,10 +17,22 @@ export function ContextMenu({ x, y, visible }: Props) {
       className="fixed bg-[#36363a] p-1.5 rounded-2xl flex flex-col gap-1 min-w-55 border border-[#424247] text-[15px]"
       style={{ left: x, top: y }}
     >
-      <Button onClick={() => {}}>Change background...</Button>
+      <Button onClick={() => swapBackground()}>Change background...</Button>
       <hr className="border-[#424247]" />
       <Button onClick={() => {}}>Display settings</Button>
       <Button onClick={() => {}}>Settings</Button>
     </div>
   );
+
+  function swapBackground() {
+    background.setBackground(
+      background.background === "/backgrounds/blobs-d.svg"
+        ? "drool"
+        : background.background === "/backgrounds/drool-d.svg"
+          ? "map"
+          : background.background === "/backgrounds/map-d.svg"
+            ? "morphogenesis"
+            : "blobs",
+    );
+  }
 }
