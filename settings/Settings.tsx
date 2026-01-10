6f2b229efa4243e20bd6d49b27774221a939946d
fabrisdev <ezepuntocom@hotmail.com>
2026-01-09 22:41:28 -0300

    Adding settings app

--- settings/Settings.tsx
+++ settings/Settings.tsx
@@ -1,7 +1,18 @@
-export function Settings() {
+import { useState } from "react";
+import { Appearance } from "./windows/Appearance";
+
+type Window = "wi-fi" | "network" | "bluetooth" | "appearance";
+
+type Props = {
+  initialWindow: Window;
+};
+
+export function Settings({ initialWindow }: Props) {
+  const [window, setWindow] = useState(initialWindow);
   return (
     <div>
-      <p>Settings</p>
+      <p>Settings - {window}</p>
+      {window === "appearance" && <Appearance />}
     </div>
   );
 }
---
+++ settings/windows/Appearance.tsx
@@ -0,0 +1,3 @@
+export function Appearance() {
+  return <p>Appareance window</p>;
+}
