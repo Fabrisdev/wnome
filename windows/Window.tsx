7762fb3d4fb58fce9e5e53efaed54d8f0a4e0783
fabrisdev <ezepuntocom@hotmail.com>
2026-01-10 01:24:43 -0300

    setting window position

--- windows/Window.tsx
+++ windows/Window.tsx
@@ -5,10 +5,18 @@ type Props = {
   className?: string;
 } & WindowProps;
 
-export function Window({ children, className }: PropsWithChildren<Props>) {
+export function Window({
+  children,
+  className,
+  position,
+}: PropsWithChildren<Props>) {
   return (
     <div
       className={`fixed bg-[#36363a] p-1.5 rounded-2xl border border-[#424247] text-[15px] ${className}`}
+      style={{
+        x: position.x,
+        y: position.y,
+      }}
     >
       {children}
     </div>
