4d7993c2bd906d11614afb6be9de18d66b72385d
fabrisdev <ezepuntocom@hotmail.com>
2026-01-10 02:38:19 -0300

    Added styles

--- settings/panels/Appearance.tsx
+++ settings/panels/Appearance.tsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+
 export function Appearance() {
-  return <p>Appareance panel</p>;
+  return (
+    <div className="flex flex-col p-20 gap-3">
+      <p className="font-bold">Style</p>
+      <div className="bg-[#343437] flex p-5 gap-5 min-w-138 justify-center rounded-xl">
+        <div>
+          <Image
+            src="/settings/appearance/style-default.png"
+            alt="Default style"
+            width={170}
+            height={127}
+          />
+          <p>Default</p>
+        </div>
+        <div>
+          <Image
+            src="/settings/appearance/style-dark.png"
+            alt="Dark style"
+            width={170}
+            height={127}
+          />
+          <p>Dark</p>
+        </div>
+      </div>
+    </div>
+  );
 }
