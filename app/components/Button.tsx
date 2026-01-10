b35b1e657684fc36724fc00f0f1427f6dda4b82c
fabrisdev <ezepuntocom@hotmail.com>
2026-01-10 16:51:34 -0300

    Allowing more customizability to button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,14 +2,19 @@ import type { PropsWithChildren } from "react";
 
 type Props = {
   onClick: () => void;
+  className?: string;
 };
 
-export function Button({ children, onClick }: PropsWithChildren<Props>) {
+export function Button({
+  children,
+  onClick,
+  className,
+}: PropsWithChildren<Props>) {
   return (
     <button
       type="button"
       onClick={onClick}
-      className="hover:bg-[#535359] p-2 rounded-xl text-left flex gap-3"
+      className={`hover:bg-[#535359] p-2 rounded-xl text-left flex gap-3 ${className}`}
     >
       {children}
     </button>
