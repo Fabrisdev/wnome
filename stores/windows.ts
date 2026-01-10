0ca57fa2427ba076425f4d78f9c1ab29f7cc8ebe
fabrisdev <ezepuntocom@hotmail.com>
2026-01-09 22:59:20 -0300

    window store

---
+++ stores/windows.ts
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+
+type App = "settings";
+type Position = {
+  x: number;
+  y: number;
+};
+type Window = {
+  id: string;
+  app: App;
+  position: Position;
+};
+
+type WindowsState = {
+  windows: Window[];
+  spawn: (app: App) => void;
+  kill: (id: string) => void;
+};
+
+export const useWindowsStore = create<WindowsState>((set) => ({
+  windows: [],
+  spawn: (app) =>
+    set((state) => ({
+      windows: [
+        ...state.windows,
+        {
+          id: crypto.randomUUID(),
+          app,
+          position: {
+            x: 0,
+            y: 0,
+          },
+        },
+      ],
+    })),
+  kill: (id) =>
+    set((state) => ({
+      windows: state.windows.filter((window) => window.id !== id),
+    })),
+}));
