23ff1ceca3e2a3bec1842196974a12c5da8dd769
fabrisdev <ezepuntocom@hotmail.com>
2026-01-09 21:07:29 -0300

    Updating context menu component to work with hook

--- app/components/ContextMenu.tsx
+++ app/components/ContextMenu.tsx
@@ -1,36 +1,16 @@
-"use client";
+type Props = {
+  x: number;
+  y: number;
+  visible: boolean;
+};
 
-import { useState } from "react";
-
-export function ContextMenu() {
-  const [menu, setMenu] = useState({
-    visible: false,
-    x: 0,
-    y: 0,
-  });
-
-  function open(event: React.MouseEvent) {
-    event.preventDefault();
-    setMenu({
-      visible: true,
-      x: event.clientX,
-      y: event.clientY,
-    });
-  }
-
-  function close() {
-    setMenu({
-      ...menu,
-      visible: false,
-    });
-  }
-
-  if (!menu.visible) return null;
+export function ContextMenu({ x, y, visible }: Props) {
+  if (!visible) return null;
 
   return (
     <div
       className="fixed bg-[#36363a] p-4 rounded-2xl flex flex-col gap-3"
-      style={{ left: menu.x, top: menu.y }}
+      style={{ left: x, top: y }}
     >
       <button>Change background...</button>
       <button>Display settings</button>
