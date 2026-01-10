9aa749d25472ae72827ecb2a1113f3295d1b1bc8
fabrisdev <ezepuntocom@hotmail.com>
2026-01-10 01:22:37 -0300

    Remove debug

--- windows/WindowManager.tsx
+++ windows/WindowManager.tsx
@@ -3,7 +3,7 @@ import { useWindowsStore } from "@/stores/windows";
 
 export function WindowManager() {
   const windows = useWindowsStore((state) => state.windows);
-  console.log(windows);
+
   return windows.map((window) => {
     if (window.app === "settings")
       return (
