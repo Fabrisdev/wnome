91389a970bfe5fd5b4d894ebdfb4ab154a2ab9dc
fabrisdev <ezepuntocom@hotmail.com>
2026-01-09 22:01:54 -0300

    reading background from store

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
+import { useBackground } from "@/hooks/useBackground";
 import { ContextMenu } from "./components/ContextMenu";
 import { useContextMenu } from "./hooks/useContextMenu";
 
 export default function Home() {
+  const { background } = useBackground();
   const { menu, open, close } = useContextMenu();
 
   return (
     <main
-      className="bg-[url(/backgrounds/blobs-d.svg)] h-svh bg-cover bg-center"
+      className="h-svh bg-cover bg-center"
       onContextMenu={open}
       onClick={close}
       onKeyUp={() => {}}
+      style={{
+        backgroundImage: `url(${background})`,
+      }}
     >
       <ContextMenu visible={menu.visible} x={menu.x} y={menu.y} />
     </main>
