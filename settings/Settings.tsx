01767caff8c809388c9342fb15cf0c38a0f80444
fabrisdev <ezepuntocom@hotmail.com>
2026-01-10 01:10:33 -0300

    Adding sidebar

--- settings/Settings.tsx
+++ settings/Settings.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Button } from "@/app/components/Button";
+import { Window } from "@/windows/Window";
 import { Appearance } from "./panels/Appearance";
 
 type Panel = "wi-fi" | "network" | "bluetooth" | "appearance";
@@ -10,9 +12,20 @@ type Props = {
 export function Settings({ initialPanel }: Props) {
   const [panel, setPanel] = useState(initialPanel);
   return (
-    <div>
-      <p>Settings - {panel}</p>
+    <Window className="flex">
+      <div>
+        <p>Settings - {panel}</p>
+        <ul>
+          <li>
+            <Button onClick={() => setPanel("wi-fi")}>Wi-Fi</Button>
+            <Button onClick={() => setPanel("network")}>Network</Button>
+            <Button onClick={() => setPanel("bluetooth")}>Bluetooth</Button>
+            <Button onClick={() => setPanel("appearance")}>Appearance</Button>
+          </li>
+        </ul>
+      </div>
+
       {panel === "appearance" && <Appearance />}
-    </div>
+    </Window>
   );
 }
