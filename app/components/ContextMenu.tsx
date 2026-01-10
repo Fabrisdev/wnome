f6c37bff266c0a358208af74554d9f5a2d249b9e
fabrisdev <ezepuntocom@hotmail.com>
2026-01-09 21:09:52 -0300

    Remove linting errors

--- app/components/ContextMenu.tsx
+++ app/components/ContextMenu.tsx
@@ -12,9 +12,15 @@ export function ContextMenu({ x, y, visible }: Props) {
       className="fixed bg-[#36363a] p-4 rounded-2xl flex flex-col gap-3"
       style={{ left: x, top: y }}
     >
-      <button>Change background...</button>
-      <button>Display settings</button>
-      <button>Settings</button>
+      <button onClick={() => {}} type="button">
+        Change background...
+      </button>
+      <button onClick={() => {}} type="button">
+        Display settings
+      </button>
+      <button onClick={() => {}} type="button">
+        Settings
+      </button>
     </div>
   );
 }
