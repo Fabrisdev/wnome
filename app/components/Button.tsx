2055298e0cc5d849f490f4ec5a6474f918ea539f
fabrisdev <ezepuntocom@hotmail.com>
2026-01-09 21:18:28 -0300

    Styles for buttons

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -6,7 +6,11 @@ type Props = {
 
 export function Button({ children, onClick }: PropsWithChildren<Props>) {
   return (
-    <button type="button" onClick={onClick}>
+    <button
+      type="button"
+      onClick={onClick}
+      className="cursor-pointer hover:bg-[#535359] p-2 rounded-2xl"
+    >
       {children}
     </button>
   );
