0107abebb0713e4597da0868e04231f143aa4c48
fabrisdev <ezepuntocom@hotmail.com>
2026-01-11 10:42:28 -0300

    you can now set your accent color

--- settings/panels/appearance/AccentColor.tsx
+++ settings/panels/appearance/AccentColor.tsx
@@ -1,12 +1,18 @@
+import { useAccentColorStore } from "@/stores/accent-color";
+
 type Props = {
   color: string;
 };
 
 export function AccentColor({ color }: Props) {
+  const setColor = useAccentColorStore((state) => state.setColor);
+
   return (
-    <div
-      className="rounded-full size-6"
-      style={{ backgroundColor: color }}
-    ></div>
+    <button type="button" onClick={() => setColor(color)}>
+      <div
+        className="rounded-full size-6"
+        style={{ backgroundColor: color }}
+      ></div>
+    </button>
   );
 }
