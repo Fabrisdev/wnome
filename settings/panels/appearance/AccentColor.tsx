886744b0799514c87fa5c2d3d5f41e889c22e184
fabrisdev <ezepuntocom@hotmail.com>
2026-01-11 10:56:57 -0300

    rendering only if its same button

--- settings/panels/appearance/AccentColor.tsx
+++ settings/panels/appearance/AccentColor.tsx
@@ -8,17 +8,23 @@ export function AccentColor({ color }: Props) {
   const setColor = useAccentColorStore((state) => state.setColor);
   const currentColor = useAccentColorStore((state) => state.color);
 
+  const button = (
+    <button
+      type="button"
+      onClick={() => setColor(color)}
+      className="rounded-full size-6"
+      style={{ backgroundColor: color }}
+    ></button>
+  );
+
+  if (currentColor !== color) return button;
+
   return (
     <div
       className="border-3 flex rounded-full justify-center items-center p-0.75"
       style={{ borderColor: currentColor }}
     >
-      <button
-        type="button"
-        onClick={() => setColor(color)}
-        className="rounded-full size-6"
-        style={{ backgroundColor: color }}
-      ></button>
+      {button}
     </div>
   );
 }
