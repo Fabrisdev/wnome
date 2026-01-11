3234011d9899517dc2c9b4c3ed19878328e64bf8
fabrisdev <ezepuntocom@hotmail.com>
2026-01-11 13:12:47 -0300

    Added checkmark on currently selected background

--- settings/panels/appearance/BackgroundOption.tsx
+++ settings/panels/appearance/BackgroundOption.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { type Background, useBackgroundStore } from "@/stores/background";
+import { Icon } from "@/ui/Icon";
 
 type Props = {
   background: Background;
@@ -7,9 +8,19 @@ type Props = {
 
 export function BackgroundOption({ background }: Props) {
   const setBackground = useBackgroundStore((state) => state.setBackground);
+  const currentBackground = useBackgroundStore((state) => state.background);
 
   return (
-    <button type="button" onClick={() => setBackground(background)}>
+    <button
+      type="button"
+      onClick={() => setBackground(background)}
+      className="relative"
+    >
+      {currentBackground === background && (
+        <div className="absolute bottom-2 right-2 size-6 rounded-full flex justify-center items-center bg-[#e62d42]">
+          <Icon name="check-plain-symbolic" />
+        </div>
+      )}
       <Image
         src={`/settings/appearance/bg-option-${background}.png`}
         alt={`${background} option background`}
