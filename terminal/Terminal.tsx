2c503decb6c86619f1ca3c2db91e4680e5d2ee44
fabrisdev <ezepuntocom@hotmail.com>
2026-01-12 02:30:44 -0300

    Added terminal

--- terminal/Terminal.tsx
+++ terminal/Terminal.tsx
@@ -1,10 +1,24 @@
+import { Terminal as OneTerminal } from "one-terminal";
 import type { WindowProps } from "@/windows/types";
 import { Window } from "@/windows/Window";
 
 export function Terminal({ id, position }: WindowProps) {
+  const vfs = {
+    kind: "directory",
+
+    entries: {
+      "readme.txt": {
+        kind: "file",
+
+        fileType: "text",
+
+        content: "Welcome to One Terminal 👋",
+      },
+    },
+  } as const;
   return (
     <Window id={id} position={position} header={<p>Terminal</p>}>
-      <p>Body</p>
+      <OneTerminal fileStructure={vfs} className="min-w-200 min-h-150" />
     </Window>
   );
 }
