23ede18c69ad4abde45853a2f6b918bd44b60f16
fabrisdev <ezepuntocom@hotmail.com>
2026-01-12 03:15:34 -0300

    using defined colors and fixed borders

--- terminal/Terminal.tsx
+++ terminal/Terminal.tsx
@@ -1,5 +1,6 @@
 import { Terminal as OneTerminal } from "one-terminal";
 import { Button } from "@/app/components/Button";
+import { useColors } from "@/settings/hooks/useColors";
 import { useWindowsStore } from "@/stores/windows";
 import { Icon } from "@/ui/Icon";
 import type { WindowProps } from "@/windows/types";
@@ -7,13 +8,17 @@ import { Window } from "@/windows/Window";
 
 export function Terminal({ id, position }: WindowProps) {
   const kill = useWindowsStore((state) => state.kill);
+  const colors = useColors();
   return (
     <Window
       id={id}
       position={position}
       className="p-0!"
       header={
-        <div className="flex justify-center items-center h-10 relative">
+        <div
+          className="flex justify-center items-center h-10 relative rounded-2xl"
+          style={{ backgroundColor: colors.terminal.header }}
+        >
           <Button noDrag onClick={() => {}} className="absolute left-2">
             <Icon name="system-search-symbolic" />
           </Button>
@@ -39,7 +44,7 @@ export function Terminal({ id, position }: WindowProps) {
       <OneTerminal
         fileStructure={{ kind: "directory", entries: {} }}
         windowChrome={{ style: "none" }}
-        theme={{ backgroundColor: "#1c1c1f" }}
+        theme={{ backgroundColor: colors.terminal.bg }}
         className="min-w-200 min-h-150 one-terminal"
       />
     </Window>
