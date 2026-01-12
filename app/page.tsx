76ab9176aaf66ea0fac3fc3ef3fb8ccc8f9127ce
fabrisdev <ezepuntocom@hotmail.com>
2026-01-12 00:52:09 -0300

    system for detecting multiple key presses

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect } from "react";
 import { useBackground } from "@/hooks/useBackground";
 import { WindowManager } from "@/windows/WindowManager";
 import { ContextMenu } from "./components/ContextMenu";
@@ -8,6 +9,25 @@ import { useContextMenu } from "./hooks/useContextMenu";
 export default function Home() {
   const { background } = useBackground();
   const { menu, open, close } = useContextMenu();
+
+  useEffect(() => {
+    const keysPressed: Record<string, true> = {};
+
+    function handleKeypress(event: KeyboardEvent) {
+      keysPressed[event.key] = true;
+    }
+
+    function releaseKey(event: KeyboardEvent) {
+      delete keysPressed[event.key];
+    }
+
+    document.addEventListener("keydown", handleKeypress);
+    document.addEventListener("keyup", releaseKey);
+    return () => {
+      document.removeEventListener("keydown", handleKeypress);
+      document.removeEventListener("keyup", releaseKey);
+    };
+  }, []);
 
   return (
     <main
