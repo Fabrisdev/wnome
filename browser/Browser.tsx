5f62daf25759d196b93c011383c9069098eb6f84
fabrisdev <ezepuntocom@hotmail.com>
2026-01-13 02:42:22 -0300

    adjusted size

--- browser/Browser.tsx
+++ browser/Browser.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useColors } from "@/settings/hooks/useColors";
 import type { WindowProps } from "@/windows/types";
 import { Window } from "@/windows/Window";
@@ -9,17 +10,21 @@ type Props = {
 
 export function Browser({ id, position, initialUrl }: Props) {
   const colors = useColors();
-  const url = initialUrl;
+  const [url, setUrl] = useState(initialUrl);
 
   return (
     <Window
       id={id}
       position={position}
       className="p-0!"
-      header={<Header id={id} />}
+      header={<Header id={id} changeUrl={setUrl} url={url} />}
       style={{ backgroundColor: colors.terminal.header }}
     >
-      <iframe src={url} className="w-full" title="Embedded Browser" />
+      <iframe
+        src={url}
+        className="min-w-250 min-h-150"
+        title="Embedded Browser"
+      />
     </Window>
   );
 }
